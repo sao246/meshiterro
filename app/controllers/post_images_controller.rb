5ee277b84b04d21db5454258be0b2b733e89d8f8
“sao246” <195246331+sao246@users.noreply.github.com>
2025-02-14 06:32:09 +0000

    [finish] 4-11 投稿一覧の作成

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -2,15 +2,15 @@ class PostImagesController < ApplicationController
   def new
     @post_image = PostImage.new
   end
-  
+  def index
+    @post_images = PostImage.all
+  end
   # 投稿データの保存
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
     @post_image.save
     redirect_to post_images_path
-  end
-  def index
   end
 
   def show
